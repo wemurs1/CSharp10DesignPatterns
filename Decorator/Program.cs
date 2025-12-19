6a4d74ea98bfd61dca5ea3d2b6a2dc1a572dbd13
Stuart Murray <stuart.murray.dev@outlook.com>
2025-12-19 15:12:37 +0000

    Decorator pattern

---
+++ Decorator/Program.cs
@@ -0,0 +1,24 @@
+﻿using Decorator;
+
+Console.Title = "Decorator";
+
+// instantiate mail services
+var cloudMailService = new CloudMailService();
+cloudMailService.SendMail("Hi there.");
+
+var onPremiseMailService = new OnPremiseMailService();
+onPremiseMailService.SendMail("Hi there.");
+
+// add behaviour
+var statisticsDecorator = new StatisticsDecorator(cloudMailService);
+statisticsDecorator.SendMail($"Hi there via {nameof(StatisticsDecorator)} wrapper.");
+
+// add behaviour
+var messageDatabaseDecorator = new MessageDatabaseDecorator(onPremiseMailService);
+messageDatabaseDecorator.SendMail($"Hi there via {nameof(MessageDatabaseDecorator)} wrapper, message 1.");
+messageDatabaseDecorator.SendMail($"Hi there via {nameof(MessageDatabaseDecorator)} wrapper, message 2.");
+
+foreach (var message in messageDatabaseDecorator.SentMessages)
+{
+    Console.WriteLine($"Stored message:\"{message}\"");
+}
