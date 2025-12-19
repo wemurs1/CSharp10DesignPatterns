7ca9b3dca68363d900ad092a593175aaf88bfe8c
Stuart Murray <stuart.murray.dev@outlook.com>
2025-12-19 09:05:58 +0000

    Builder pattern

---
+++ Builder/Program.cs
@@ -0,0 +1,20 @@
+﻿using Builder;
+
+Console.Title = "Builder";
+
+var garage = new Garage();
+
+var minBuilder = new MiniBuilder();
+var bmwBuilder = new BMWBuilder();
+
+garage.Construct(minBuilder);
+Console.WriteLine(minBuilder.Car.ToString());
+// or:
+garage.Show();
+
+garage.Construct(bmwBuilder);
+Console.WriteLine(bmwBuilder.Car.ToString());
+// or:
+garage.Show();
+
+Console.ReadKey();
