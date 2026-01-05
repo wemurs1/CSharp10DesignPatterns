20c544c528611572e2a96c819b9f2f476c4bdba9
Stuart Murray <stuart.murray.dev@outlook.com>
2026-01-05 12:26:45 +0000

    flyweight pattern

---
+++ FlyWeight/Program.cs
@@ -0,0 +1,32 @@
+﻿using FlyWeight;
+
+Console.Title = "Fly Weight";
+
+var aBunchOrCharacters = "abba";
+
+var characterFactory = new CharacterFactory();
+
+// Get the flyweight(s)
+var characterObject = characterFactory.GetCharacter(aBunchOrCharacters[0]);
+// Pass through extrinsic state
+characterObject?.Draw("Arial", 12);
+
+characterObject = characterFactory.GetCharacter(aBunchOrCharacters[1]);
+characterObject?.Draw("Trebuchet MS", 14);
+
+characterObject = characterFactory.GetCharacter(aBunchOrCharacters[2]);
+characterObject?.Draw("Times New Roman", 16);
+
+characterObject = characterFactory.GetCharacter(aBunchOrCharacters[3]);
+characterObject?.Draw("Comic Sans", 18);
+
+// create unshared concreet flyweight (paragraph)
+var paragraph = characterFactory.CreateParagraph(
+    new List<ICharacter> { characterObject! },
+    1
+);
+
+// draw the paragraph
+paragraph.Draw("Lucinda", 12);
+
+Console.ReadKey();
