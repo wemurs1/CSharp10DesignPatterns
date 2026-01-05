e4c45cb6a505363733a1344667db472ca848048c
Stuart Murray <stuart.murray.dev@outlook.com>
2026-01-05 17:20:23 +0000

    Command Pattern

---
+++ Command/Program.cs
@@ -0,0 +1,22 @@
+﻿using Command;
+
+Console.Title = "Command Pattern";
+
+CommandManager commandManager = new();
+IEmployeeManagerRepository repository = new EmployeeManagerRepository();
+
+commandManager.Invoke(new AddEmployeeToManagerList(repository, 1, new Employee(111, "Kevin")));
+repository.WriteDataStore();
+
+commandManager.Invoke(new AddEmployeeToManagerList(repository, 1, new Employee(222, "Clara")));
+repository.WriteDataStore();
+
+commandManager.Invoke(new AddEmployeeToManagerList(repository, 2, new Employee(333, "Tom")));
+repository.WriteDataStore();
+
+
+// try adding an employee a second time
+commandManager.Invoke(new AddEmployeeToManagerList(repository, 2, new Employee(333, "Tom")));
+repository.WriteDataStore();
+
+Console.ReadKey();
