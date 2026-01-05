36db648534b0fa47b196c9506b363e1e6df06bf3
Stuart Murray <stuart.murray.dev@outlook.com>
2026-01-05 14:57:33 +0000

    Strategy Pattern

---
+++ Strategy/Program.cs
@@ -0,0 +1,12 @@
+﻿using Strategy;
+
+Console.Title = "Strategy Pattern";
+
+var order = new Order("Marvin Software", 5, "Visual Studio License");
+order.ExportService = new CSVExportService();
+order.Export();
+
+order.ExportService = new JsonExportService();
+order.Export();
+
+Console.ReadKey();
