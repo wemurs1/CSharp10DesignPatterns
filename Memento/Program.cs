a88928ad6b868c3ecb4c636312db9b94f51f95d7
Stuart Murray <stuart.murray.dev@outlook.com>
2026-01-06 10:37:28 +0000

    basic memento

---
+++ Memento/Program.cs
@@ -0,0 +1,28 @@
+﻿using Memento;
+
+Console.Title = "Memento";
+
+CommandManager commandManager = new();
+IEmployeeManagerRepository repository = new EmployeeManagerRepository();
+
+commandManager.Invoke(new AddEmployeeToManagerList(repository, 1, new Employee(111, "Kevin")));
+repository.WriteDataStore();
+
+commandManager.Undo();
+repository.WriteDataStore();
+
+commandManager.Invoke(new AddEmployeeToManagerList(repository, 1, new Employee(222, "Clara")));
+repository.WriteDataStore();
+
+commandManager.Invoke(new AddEmployeeToManagerList(repository, 2, new Employee(333, "Tom")));
+repository.WriteDataStore();
+
+
+// try adding an employee a second time
+commandManager.Invoke(new AddEmployeeToManagerList(repository, 2, new Employee(333, "Tom")));
+repository.WriteDataStore();
+
+commandManager.UndoAll();
+repository.WriteDataStore();
+
+Console.ReadKey();
