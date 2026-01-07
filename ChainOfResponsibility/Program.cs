6cad55bb19e3e72ad354a79f1f807800a81a06f1
Stuart Murray <stuart.murray.dev@outlook.com>
2026-01-07 08:29:23 +0000

    Chain Of Responsibility

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ChainOfResponsibility;
+
+Console.Title = "Chain of Responsibility";
+
+var validDocument = new Document("Hot to Avoid Java Development", DateTimeOffset.UtcNow, true, true);
+var invalidDocument = new Document("Hot to Avoid Java Development", DateTimeOffset.UtcNow, false, true);
+
+var documentHandlerChain = new DocumentTitleHandler();
+documentHandlerChain
+    .SetSuccessor(new DocumentLastModifiedHandler())
+    .SetSuccessor(new DocumentApprovedByLitigationHandler())
+    .SetSuccessor(new DocumentApprovedByManagementHandler());
+
+try
+{
+    documentHandlerChain.Handle(validDocument);
+    Console.WriteLine("Valid document is valid");
+    documentHandlerChain.Handle(invalidDocument);
+    Console.WriteLine("Invalid document is valid");
+}
+catch (ValidationException validationException)
+{
+    Console.WriteLine(validationException.Message);
+}
+
+Console.ReadKey();
