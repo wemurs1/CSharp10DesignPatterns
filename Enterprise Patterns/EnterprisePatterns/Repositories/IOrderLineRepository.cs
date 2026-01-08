53507a1c8cbf9a3843de1eae73ea8c680e121e83
Stuart Murray <stuart.murray.dev@outlook.com>
2026-01-08 17:04:58 +0000

    Emherit specific repository from generic repository

--- Enterprise Patterns/EnterprisePatterns/Repositories/IOrderLineRepository.cs
+++ Enterprise Patterns/EnterprisePatterns/Repositories/IOrderLineRepository.cs
@@ -2,13 +2,18 @@ using EnterprisePatterns.Entities;
 
 namespace EnterprisePatterns.Repositories;
 
-public interface IOrderLineRepository
+// public interface IOrderLineRepository
+// {
+//     Task<OrderLine?> Get(int id);
+//     Task<IEnumerable<OrderLine>> GetAll();
+//     Task<IEnumerable<OrderLine>> GetAllByOrderId(int orderId);
+//     void Add(OrderLine entity);
+//     void Delete(OrderLine entity);
+//     void Update(OrderLine entity);
+//     Task SaveChanges();
+// }
+
+public interface IOrderLineRepository : IRepository<OrderLine>
 {
-    Task<OrderLine?> Get(int id);
-    Task<IEnumerable<OrderLine>> GetAll();
     Task<IEnumerable<OrderLine>> GetAllByOrderId(int orderId);
-    void Add(OrderLine entity);
-    void Delete(OrderLine entity);
-    void Update(OrderLine entity);
-    Task SaveChanges();
 }
